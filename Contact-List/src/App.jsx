c8d1d01fd7888262763cae8f38387651ce5a4427
Sophia Lin <SophiaCPLin@Sophias-MacBook.hitronhub.home>
2024-08-08 18:58:43 +0800

    finished setting up table

--- Contact-List/src/App.jsx
+++ Contact-List/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import ContactList from './components/ContactList'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App() {
+
 
   return (
     <>
-
+    <ContactList/>
     </>
   )
 }
 
-export default App
